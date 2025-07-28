8ecd5c112afba648520672ab9621b55e90089d42
ClemersonPaschoa <89163945+ClemersonPaschoa@users.noreply.github.com>
2025-07-28 10:55:58 -0300

    Update CartasSuperTrunfo.c
    Comparando cartas com super poder

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,6 +10,8 @@ int main (){
     char codigo[10] = "A01", codigo1[10] = "B01";
     char cidade[20] = "", cidade1[20] = "";
     float densidade, ppc, densidade1, ppc1;
+    float SuperPoder, SuperPoder1;
+    float respop, resarea, respib, respontos, resdens, ressuper, resppc;
 
     // Solicitando a entrada de dados para cada informação da carta 1
     printf ("Carta 1\n");
@@ -45,6 +47,7 @@ int main (){
     // Armazenando o resultado nas variaveis
     densidade = (float) populacao/area;
     ppc = (float) pib/populacao;
+    SuperPoder = populacao + area + pib + pontostur + ppc + (1/densidade);
 
 
     // Solicitando a entrada de dados para cada informação da carta 2
@@ -80,6 +83,17 @@ int main (){
     // Armazenando o resultado nas variaveis
     densidade1 = (float) populacao1/area1;
     ppc1 = (float) pib1/populacao1;
+    SuperPoder1 = populacao1 + area1 + pib1 + pontostur1 + ppc1 + (1/densidade1);
+
+
+    //Comparação dos resultados
+    resarea = area > area1;
+    resdens = densidade > densidade1;
+    respib = pib > pib1;
+    respontos = pontostur > pontostur1;
+    respop = populacao > populacao1;
+    ressuper = SuperPoder > SuperPoder1;
+    resppc = ppc > ppc1;
 
     // imprime os dados da carta 1
     printf ("Carta 1\n");
@@ -92,6 +106,7 @@ int main (){
     printf ("Número de pontos turísticos: %d\n", pontostur);
     printf ("Densidade Populacional: %.2f\n", densidade);
     printf ("PIB per capita: %.2f\n", ppc);
+    printf ("Super poder: %.2f\n", SuperPoder);
 
     // imprime os dados da carta 2
     printf ("Carta 2\n");
@@ -104,6 +119,18 @@ int main (){
     printf ("Número de pontos turísticos: %d\n", pontostur1);
     printf ("Densidade Populacional: %.2f\n", densidade1);
     printf ("PIB per capita: %.2f\n", ppc1);
+    printf ("Super poder: %.2f\n", SuperPoder1);
+
+    // Comparação das cartas
+    printf ("\nComparação das Cartas:\n");
+    printf ("\nCarta 1 venceu se resultado for 1, e Carta 2 venceu se resultado for 0\n");
+    printf ("\nPopulação = %d\n", (int)respop);
+    printf ("Área = %d\n", (int)resarea);
+    printf ("PIB = %d\n", (int)respib);
+    printf ("Pontos Turísticos = %d\n", (int)respontos);
+    printf ("Densidade Populacional = %d\n", (int)resdens);
+    printf ("PIB Per Capita = %d\n", (int)resppc);
+    printf ("Super Poder = %d\n", (int)ressuper);
 
     return 0;
 }
